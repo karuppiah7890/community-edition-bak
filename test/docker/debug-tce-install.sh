840590a86d28347fbaf57664051b4c42145151b8
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2021-07-09 00:34:35 +0530

    add info comments

--- test/docker/debug-tce-install.sh
+++ test/docker/debug-tce-install.sh
@@ -120,6 +120,10 @@ do
         is_ready=$(kubectl get pod -n "${namespace}" "${name}" -o json | jq '.status.conditions[] | select(.type == "Ready") | .status' -r)
 
         if [[ "${is_ready}" != "True" ]]; then
+            # The below line is not change the value of `all_ready` to affect the
+            # if statement that comes later. Maybe it's because the `all_ready` is used
+            # in a nested block / context? Hmm. The definition is outside the if block and
+            # outside the while loop over here
             all_ready="false"
             kubectl describe pod -n "${namespace}" "${name}" | tail
             kubectl logs -n "${namespace}" "${name}" || true
