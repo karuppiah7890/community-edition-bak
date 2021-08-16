88b94de18addad5d34b47f15da6947b53f03e40a
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-08-16 13:20:09 +0530

    package darwin tar ball
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- hack/package-release.sh
+++ hack/package-release.sh
@@ -118,11 +118,11 @@ chown -R "$USER":"$(id -g -n "$USER")" "${PACKAGE_DARWIN_AMD64_DIR}"
 # chown -R "$USER":"$(id -g -n "$USER")" "${PACKAGE_WINDOWS_AMD64_DIR}"
 
 # packaging
-rm -f tce-linux-amd64-*.tar.gz
-# rm -f tce-darwin-amd64-*.tar.gz
+# rm -f tce-linux-amd64-*.tar.gz
+rm -f tce-darwin-amd64-*.tar.gz
 # rm -f tce-windows-amd64-*.tar.gz
 pushd "${BUILD_ROOT_DIR}" || exit 1
-tar -czvf "tce-linux-amd64-${TCE_BUILD_VERSION}.tar.gz" "tce-linux-amd64-${BUILD_VERSION}"
-# tar -czvf "tce-darwin-amd64-${TCE_BUILD_VERSION}.tar.gz" "tce-darwin-amd64-${BUILD_VERSION}"
+# tar -czvf "tce-linux-amd64-${TCE_BUILD_VERSION}.tar.gz" "tce-linux-amd64-${BUILD_VERSION}"
+tar -czvf "tce-darwin-amd64-${TCE_BUILD_VERSION}.tar.gz" "tce-darwin-amd64-${BUILD_VERSION}"
 # tar -czvf "tce-windows-amd64-${TCE_BUILD_VERSION}.tar.gz" "tce-windows-amd64-${BUILD_VERSION}"
 popd || exit 1
