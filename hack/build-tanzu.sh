9a388d9e5cd51d288a88406ecd124706fa9a6d86
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-08-17 10:51:00 +0530

    match with main
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- hack/build-tanzu.sh
+++ hack/build-tanzu.sh
@@ -58,17 +58,17 @@ fi
 # BUILD_SHA=${BUILD_SHA} BUILD_VERSION=${FRAMEWORK_BUILD_VERSION} make build-install-cli-all ENVS=linux-amd64
 BUILD_SHA=${BUILD_SHA} BUILD_VERSION=${FRAMEWORK_BUILD_VERSION} make build-install-cli-all ENVS=darwin-amd64
 
-make clean-catalog-cache
-make clean-cli-plugins
+# make clean-catalog-cache
+# make clean-cli-plugins
 # BUILD_SHA=${BUILD_SHA} BUILD_VERSION=${FRAMEWORK_BUILD_VERSION} make build-plugin-admin ENVS=linux-amd64
 BUILD_SHA=${BUILD_SHA} BUILD_VERSION=${FRAMEWORK_BUILD_VERSION} make build-plugin-admin
 
 # ./hack/embed-pinniped-binary.sh go linux amd64
 # GOPRIVATE="github.com/vmware-tanzu/tanzu-framework" go run ./cmd/cli/plugin-admin/builder/main.go cli compile --version ${FRAMEWORK_BUILD_VERSION} --ldflags "-s -w -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildDate=2021-08-06' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildSHA=5a42a2b' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildVersion=v1.4.0-pre-alpha-2' -X 'main.BuildEdition=tce' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/buildinfo.IsOfficialBuild=""'" --tags "" --corepath "cmd/cli/tanzu" --artifacts artifacts/linux/amd64/cli --target  linux_amd64 --match "*management*"
-GOPRIVATE="github.com/vmware-tanzu/tanzu-framework" go run ./cmd/cli/plugin-admin/builder/main.go cli compile --version ${FRAMEWORK_BUILD_VERSION} --ldflags "-s -w -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildDate=2021-08-06' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildSHA=5a42a2b' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildVersion=v1.4.0-pre-alpha-2' -X 'main.BuildEdition=tce' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/buildinfo.IsOfficialBuild=""'" --tags "" --corepath "cmd/cli/tanzu" --artifacts artifacts/darwin/amd64/cli --target  darwin_amd64 --match "*management*"
+# GOPRIVATE="github.com/vmware-tanzu/tanzu-framework" go run ./cmd/cli/plugin-admin/builder/main.go cli compile --version ${FRAMEWORK_BUILD_VERSION} --ldflags "-s -w -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildDate=2021-08-06' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildSHA=5a42a2b' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli.BuildVersion=v1.4.0-pre-alpha-2' -X 'main.BuildEdition=tce' -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/buildinfo.IsOfficialBuild=""'" --tags "" --corepath "cmd/cli/tanzu" --artifacts artifacts/darwin/amd64/cli --target  darwin_amd64 --match "*management*"
 
-make install-cli-plugins
-make install-cli
+# make install-cli-plugins
+# make install-cli
 
 # by default, tanzu-framework only builds admins plugins for the current platform. we need darwin also.
 # BUILD_VERSION=${FRAMEWORK_BUILD_VERSION} GOHOSTOS=linux GOHOSTARCH=amd64 make build-plugin-admin
