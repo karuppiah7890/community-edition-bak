ff7f8face517db656cc2873c1fd43def3a56f3f5
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-08-24 16:46:56 +0530

    [e2e][vsphere] add e2e test for vsphere management + workload cluster
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/docker/check-tce-cluster-creation.sh
+++ test/docker/check-tce-cluster-creation.sh
@@ -3,20 +3,43 @@
 # Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set -e # Fail on errors
-set -x # See what commands are running
+set -e
+set -x
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TCE_REPO_PATH="${MY_DIR}"/../..
+
+# shellcheck source=test/util/utils.sh
+source "${TCE_REPO_PATH}"/test/util/utils.sh
 
 kube_context=$1
 
 if [ -z "$kube_context" ]; then
-    echo "Error: Kube context name not provided. Please provide kube context name"
+    error "Error: Kube context name not provided. Please provide kube context name"
     exit 1
 fi
 
-kubectl config use-context "$kube_context"
+kubectl config use-context "$kube_context" || {
+    error "CONTEXT SWITCH TO CLUSTER FAILED!"
+    exit 1
+}
 
-kubectl cluster-info
+kubectl wait --for=condition=ready pod --all --all-namespaces --timeout=600s || {
+    error "TIMED OUT WAITING FOR ALL PODS TO BE UP!"
+    exit 1
+}
 
-kubectl get nodes
+kubectl cluster-info || {
+    error "ERROR GETTING CLUSTER INFO!"
+    exit 1
+}
 
-kubectl get pods -A
+kubectl get nodes || {
+    error "ERROR GETTING CLUSTER NODES!"
+    exit 1
+}
+
+kubectl get pods -A || {
+    error "ERROR GETTING ALL PODS IN CLUSTER!"
+    exit 1
+}
