b872ffe36f73e7808987937b5aae646d3450da20
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-08-27 18:18:10 +0530

    remove doc on unnecessary environment variables needed for run proxy script
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/vsphere/run-proxy-to-vcenter-server-and-control-plane.sh
+++ test/vsphere/run-proxy-to-vcenter-server-and-control-plane.sh
@@ -8,8 +8,6 @@ set -x
 
 # Note: This script supports only Linux(Debian/Ubuntu) and MacOS
 # Following environment variables are expected to be exported before running the script
-# VSPHERE_CONTROL_PLANE_ENDPOINT - virtual and static IP for the cluster's control plane nodes
-# VSPHERE_SERVER - private IP of the vcenter server
 # JUMPER_SSH_HOST_IP - public IP address to access the Jumper host for SSH
 # JUMPER_SSH_USERNAME - username to access the Jumper host for SSH
 # JUMPER_SSH_PRIVATE_KEY - private key to access to access the Jumper host for SSH
