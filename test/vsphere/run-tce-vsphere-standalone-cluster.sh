dc393fb03053b5c70765987a604ffb1874f52719
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-08-30 17:50:19 +0530

    inline the deletecluster function in vsphere standalone cluster e2e test
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/vsphere/run-tce-vsphere-standalone-cluster.sh
+++ test/vsphere/run-tce-vsphere-standalone-cluster.sh
@@ -61,25 +61,6 @@ trap '{ "${MY_DIR}"/stop-proxy-to-vcenter-server-and-control-plane.sh; }' EXIT
 
 cluster_config_file="${MY_DIR}"/standalone-cluster-config.yaml
 
-# Cleanup function
-function deletecluster {
-    vsphere_cluster_name=$1
-
-    if [[ -z "${vsphere_cluster_name}" ]]; then
-        echo "Cluster name not passed to deletecluster function. Usage example: deletecluster standalone-cluster-1234"
-        exit 1
-    fi
-
-    echo "Deleting standalone cluster"
-    tanzu standalone-cluster delete ${vsphere_cluster_name} -y || {
-        error "STANDALONE CLUSTER DELETION FAILED!"
-        govc_cleanup ${vsphere_cluster_name}
-        # Finally fail after cleanup because cluster delete command failed,
-        # and cluster delete command is a subject under test (SUT) in the E2E test
-        exit 1
-    }
-}
-
 tanzu standalone-cluster create ${CLUSTER_NAME} --file "${cluster_config_file}" -v 10 || {
     error "STANDALONE CLUSTER CREATION FAILED!"
     govc_cleanup ${CLUSTER_NAME}
@@ -91,4 +72,12 @@ tanzu standalone-cluster create ${CLUSTER_NAME} --file "${cluster_config_file}" 
 "${MY_DIR}"/../docker/check-tce-cluster-creation.sh ${CLUSTER_NAME}-admin@${CLUSTER_NAME}
 
 echo "Cleaning up"
-deletecluster ${CLUSTER_NAME}
+echo "Deleting standalone cluster"
+
+tanzu standalone-cluster delete ${CLUSTER_NAME} -y || {
+    error "STANDALONE CLUSTER DELETION FAILED!"
+    govc_cleanup ${CLUSTER_NAME}
+    # Finally fail after cleanup because cluster delete command failed,
+    # and cluster delete command is a subject under test (SUT) in the E2E test
+    exit 1
+}
