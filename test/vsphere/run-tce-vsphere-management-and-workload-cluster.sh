8b7aa9a21adb1f87f68a8d02f055647d11154904
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-08-31 12:49:16 +0530

    add todo comments
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
+++ test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
@@ -112,7 +112,8 @@ management_cluster_config_file="${MY_DIR}"/management-cluster-config.yaml
 
 tanzu management-cluster create ${CLUSTER_NAME} --file "${management_cluster_config_file}" -v 10 || {
     error "MANAGEMENT CLUSTER CREATION FAILED!"
-    # TODO: what if govc_cleanup fails? It just stops? it's okay?
+    # TODO: what if govc_cleanup fails? It just stops? it's okay I guess? however it's gonna stop, with exit 1, hmm.
+    # but we need error messages here!
     govc_cleanup ${CLUSTER_NAME}
     # Finally fail after cleanup because cluster create command failed,
     # and cluster create command is a subject under test (SUT) in the E2E test
