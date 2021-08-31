f3f86f0e82866c273a43116255520d6facfd7888
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-08-31 12:56:06 +0530

    update error messages
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
+++ test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
@@ -80,7 +80,7 @@ function delete_management_cluster {
 
     echo "Deleting management cluster"
     tanzu management-cluster delete ${vsphere_cluster_name} -y || {
-        error "MANAGEMENT CLUSTER DELETION FAILED!"
+        error "MANAGEMENT CLUSTER DELETION FAILED!! Using govc to cleanup cluster resources"
         # TODO: what if govc_cleanup fails? It just stops? it's okay?
         govc_cleanup ${vsphere_cluster_name}
         # Finally fail after cleanup because cluster delete command failed,
@@ -99,7 +99,7 @@ function delete_workload_cluster {
 
     echo "Deleting workload cluster"
     tanzu cluster delete ${vsphere_cluster_name} -y || {
-        error "WORKLOAD CLUSTER DELETION FAILED!"
+        error "WORKLOAD CLUSTER DELETION FAILED!! Using govc to cleanup cluster resources"
         # TODO: what if govc_cleanup fails? It just stops? it's okay?
         govc_cleanup ${vsphere_cluster_name}
         # Finally fail after cleanup because cluster delete command failed,
