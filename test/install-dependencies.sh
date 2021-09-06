5970e4ee6a384aa6d3448c452c1db3f429018209
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-09-06 14:17:17 +0530

    some cleanup
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/install-dependencies.sh
+++ test/install-dependencies.sh
@@ -26,9 +26,9 @@ if [[ -z "$(command -v docker)" ]]; then
             curl \
             gnupg \
             lsb-release > /dev/null 2>&1
-        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo \
-            "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+            "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
             $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
         sudo apt update > /dev/null
