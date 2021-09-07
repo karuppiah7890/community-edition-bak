a3609989a79250e48a6c5d5fc3131dba589778cd
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-09-07 20:45:14 +0530

    run vsphere e2e tests for standalone and management clusters on e2e-vsphere-management-cluster-trial-2 trial branch
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>
    
    add script to fetch and install a released TCE release from GitHub
    
    print tce standalone cluster plugin version and show errors
    
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>
    
    run vsphere e2e tests for standalone and management clusters on e2e-vsphere-management-cluster-trial-3--with-stable-binary using tce v0.7.0 stable binary
    
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>
    
    fix token name
    
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>
    
    fix token name
    
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
+++ test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
@@ -46,7 +46,7 @@ source "${TCE_REPO_PATH}"/test/util/utils.sh
 source "${TCE_REPO_PATH}"/test/vsphere/cleanup-utils.sh
 
 "${TCE_REPO_PATH}"/test/install-dependencies.sh || { error "Dependency installation failed!"; exit 1; }
-"${TCE_REPO_PATH}"/test/build-tce.sh || { error "TCE installation failed!"; exit 1; }
+"${TCE_REPO_PATH}"/test/fetch-and-install-tce-release.sh v0.7.0 || { error "TCE installation failed!"; exit 1; }
 
 random_id="${RANDOM}"
 
