52e7726f9599fb583da9168ed13944da74c22671
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-09-13 13:22:56 +0530

    handle failures in az CLI and split tests into functions
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/azure/deploy-standalone-cluster.sh
+++ test/azure/deploy-standalone-cluster.sh
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # This script tests TCE Standalone cluster in Azure.
-# It builds TCE, spins up a standalone cluster in Azure, 
+# It builds TCE, spins up a standalone cluster in Azure,
 # installs the default packages,
 # and cleans the environment.
 # Note: This script supports only Linux(Debian) and MacOS
@@ -49,51 +49,106 @@ function az_docker {
 }
 
 function azure_cluster_cleanup {
+    failure_message="STANDLONE CLUSTER CLEANUP USING azure CLI FAILED! Please manually delete any ${CLUSTER_NAME} standalone cluster resources using Azure Web UI"
     echo "Cleaning up ${CLUSTER} cluster resources using azure CLI"
 
     az_docker login --service-principal --username "${AZURE_CLIENT_ID}" --password "${AZURE_CLIENT_SECRET}" \
-        --tenant "${AZURE_TENANT_ID}"
+        --tenant "${AZURE_TENANT_ID}" || {
+        error "azure CLI LOGIN FAILED!"
+        error ${failure_message}
+        return 1
+    }
 
-    az_docker account set --subscription "${AZURE_SUBSCRIPTION_ID}"
+    az_docker account set --subscription "${AZURE_SUBSCRIPTION_ID}" || {
+        error "azure CLI SETTING ACCOUNT SUBSCRIPTION ID FAILED!"
+        error ${failure_message}
+        return 1
+    }
 
-    az_docker group delete --name "${AZURE_RESOURCE_GROUP}" --yes
+    az_docker group delete --name "${AZURE_RESOURCE_GROUP}" --yes || {
+        error "azure CLI RESOURCE GROUP DELETION FAILED!"
+        error ${failure_message}
+        return 1
+    }
 }
 
-function delete_cluster {
+function delete_cluster_or_cleanup {
     echo "Deleting standalone cluster"
-    tanzu standalone-cluster delete ${CLUSTER_NAME} -y
+    tanzu standalone-cluster delete ${CLUSTER_NAME} -y || {
+        error "STANDALONE CLUSTER DELETION FAILED!";
+        azure_cluster_cleanup
+        return 1
+    }
 }
 
 function create_standalone_cluster {
     echo "Bootstrapping TCE standalone cluster on AWS..."
-    tanzu standalone-cluster create "${CLUSTER_NAME}" -f "${TCE_REPO_PATH}"/test/aws/cluster-config.yaml || { 
+    tanzu standalone-cluster create "${CLUSTER_NAME}" -f "${TCE_REPO_PATH}"/test/aws/cluster-config.yaml || {
         error "STANDALONE CLUSTER CREATION FAILED!";
-        delete_kind_cluster;
-        aws-nuke-tear-down "${CLUSTER_NAME}";
-        exit 1;
+        return 1;
     }
-    kubectl config get-contexts "${CLUSTER_NAME}"-admin@"${CLUSTER_NAME}" || { 
-        error "CONTEXT NOT PRESENT IN KUBECONFIG FOR STANDALONE CLUSTER!";
-        delete_cluster;
-        exit 1;
-    }
-
-    kubectl wait --for=condition=ready pod --all --all-namespaces --timeout=300s || { error "TIMED OUT WAITING FOR ALL PODS TO BE UP!"; delete_cluster; exit 1; }
 }
 
-create_standalone_cluster
+function wait_for_pods {
+    kubectl config use-context "${CLUSTER_NAME}"-admin@"${CLUSTER_NAME}" || {
+        error "CONTEXT SWITCH TO STANDALONE CLUSTER FAILED!";
+        return 1;
+    }
+    kubectl wait --for=condition=ready pod --all --all-namespaces --timeout=300s || {
+        error "TIMED OUT WAITING FOR ALL PODS TO BE UP!";
+        return 1;
+    }
+}
 
-echo "Installing packages on TCE..."
-"${TCE_REPO_PATH}"/test/add-tce-package-repo.sh || { error "PACKAGE REPOSITORY INSTALLATION FAILED!"; delete_cluster; exit 1; }
-tanzu package available list || { error "UNEXPECTED FAILURE OCCURRED!"; delete_cluster; exit 1; }
+function add_package_repo {
+    echo "Installing package repository on TCE..."
+    "${TCE_REPO_PATH}"/test/add-tce-package-repo.sh || {
+        error "PACKAGE REPOSITORY INSTALLATION FAILED!";
+        return 1;
+    }
+}
 
-# Run e2e-test
-echo "Starting Gatekeeper test..."
-"${TCE_REPO_PATH}"/test/aws/e2e-test.sh || { error "TEST FAILED!"; delete_cluster "Deleting standalone cluster"; exit 1; }
+function list_packages {
+    tanzu package available list || {
+        error "LISTING PACKAGES FAILED";
+        return 1;
+    }
+}
 
-echo "Cleaning up..."
-delete_cluster || {
-    error "STANDALONE CLUSTER DELETION FAILED!";
-    azure_cluster_cleanup;
+function test_gate_keeper_package {
+    echo "Starting Gatekeeper test..."
+    "${TCE_REPO_PATH}"/test/aws/e2e-test.sh || {
+        error "GATEKEEPER PACKAGE TEST FAILED!";
+        return 1;
+    }
+}
+
+create_standalone_cluster || {
+    delete_kind_cluster
+    azure_cluster_cleanup
     exit 1
 }
+
+wait_for_pods || {
+    delete_cluster_or_cleanup
+    exit 1
+}
+
+
+add_package_repo || {
+    delete_cluster_or_cleanup
+    exit 1
+}
+
+list_packages || {
+    delete_cluster_or_cleanup
+    exit 1
+}
+
+test_gate_keeper_package || {
+    delete_cluster_or_cleanup
+    exit 1
+}
+
+echo "Cleaning up..."
+delete_cluster_or_cleanup
