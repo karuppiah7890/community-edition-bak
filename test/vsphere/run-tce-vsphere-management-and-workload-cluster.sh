c86b3ffea732e9b0271502036de8f3717cc9893f
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-09-17 17:27:56 +0530

    run vsphere management + workload cluster e2e on tce v0.8.0-rc.3 in e2e-vsphere-mc-trial-4 branch
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
+++ test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
@@ -48,7 +48,7 @@ source "${TCE_REPO_PATH}"/test/vsphere/cleanup-utils.sh
 "${TCE_REPO_PATH}"/test/install-dependencies.sh || { error "Dependency installation failed!"; exit 1; }
 # "${TCE_REPO_PATH}"/test/build-tce.sh || { error "TCE installation failed!"; exit 1; }
 
-"${TCE_REPO_PATH}"/test/fetch-and-install-tce-release.sh v0.8.0-rc.2
+"${TCE_REPO_PATH}"/test/fetch-and-install-tce-release.sh v0.8.0-rc.3
 
 random_id="${RANDOM}"
 
