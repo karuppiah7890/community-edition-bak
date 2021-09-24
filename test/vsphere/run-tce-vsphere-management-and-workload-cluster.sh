0533c623bf98ffee099af133cf19913bc578a15c
Karuppiah Natarajan <karuppiahn@vmware.com>
2021-09-24 13:37:32 +0530

    run vsphere standalone and management + workload cluster test on TCE v0.9.0-rc.1
    Signed-off-by: Karuppiah Natarajan <karuppiahn@vmware.com>

--- test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
+++ test/vsphere/run-tce-vsphere-management-and-workload-cluster.sh
@@ -46,7 +46,9 @@ source "${TCE_REPO_PATH}"/test/util/utils.sh
 source "${TCE_REPO_PATH}"/test/vsphere/cleanup-utils.sh
 
 "${TCE_REPO_PATH}"/test/install-dependencies.sh || { error "Dependency installation failed!"; exit 1; }
-"${TCE_REPO_PATH}"/test/build-tce.sh || { error "TCE installation failed!"; exit 1; }
+# "${TCE_REPO_PATH}"/test/build-tce.sh || { error "TCE installation failed!"; exit 1; }
+
+"${TCE_REPO_PATH}"/test/fetch-and-install-tce-release.sh v0.9.0-rc.1
 
 random_id="${RANDOM}"
 
